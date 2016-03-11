a3331a4ccb4d2cc22a20067293b482ec4eecbfd6
Actimia <actimia1@gmail.com>
2016-03-11 16:28:49 +0100

    Extracted natives and updated gitignore, added basic game code.

--- src/tdfpro/twitchplays/Main.java
+++ src/tdfpro/twitchplays/Main.java
@@ -1,7 +1,30 @@
 package tdfpro.twitchplays;
 
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("hej world");
+import org.newdawn.slick.*;
+
+public class Main extends BasicGame {
+	public Main() {
+		super("Twitch Plays Hangman - by the TDF Team");
+	}
+
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer agc = new AppGameContainer(new Main());
+		agc.setDisplayMode(1024, 720, false);
+		agc.start();
+	}
+
+	@Override
+	public void init(GameContainer gameContainer) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer gameContainer, int i) throws SlickException {
+
+	}
+
+	@Override
+	public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+		graphics.drawString("Hello, world", 100, 100);
 	}
 }
